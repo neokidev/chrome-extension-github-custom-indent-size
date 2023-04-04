18bf7a5420c1858d9e6b056390bf94d3708447dd
ot07 <ot07neoki@gmail.com>
2023-04-05 00:20:22 +0900

    update manifest and vite config

--- manifest.ts
+++ manifest.ts
@@ -8,30 +8,19 @@ const manifest: chrome.runtime.ManifestV3 = {
   name: "GitHub Custom Indent Size",
   version: packageJson.version,
   description: packageJson.description,
-  options_page: "src/pages/options/index.html",
-  background: {
-    service_worker: "src/pages/background/index.js",
-    type: "module",
-  },
   action: {
     default_popup: "src/pages/popup/index.html",
     default_icon: "icon-34.png",
-  },
-  chrome_url_overrides: {
-    newtab: "src/pages/newtab/index.html",
   },
   icons: {
     "128": "icon-128.png",
   },
   content_scripts: [
     {
-      matches: ["http://*/*", "https://*/*", "<all_urls>"],
+      matches: ["https://github.com/*"],
       js: ["src/pages/content/index.js"],
-      // KEY for cache invalidation
-      css: ["assets/css/contentStyle<KEY>.chunk.css"],
     },
   ],
-  devtools_page: "src/pages/devtools/index.html",
   web_accessible_resources: [
     {
       resources: [
@@ -43,6 +32,7 @@ const manifest: chrome.runtime.ManifestV3 = {
       matches: ["*://*/*"],
     },
   ],
+  permissions: ["storage"],
 };
 
 export default manifest;
