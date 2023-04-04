5344e9f0e15d0c1188fa02d5f57ff86c4baf3a64
ot07 <ot07neoki@gmail.com>
2023-04-05 00:36:29 +0900

    add popup to change tab size

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import logo from "@assets/img/logo.svg";
 import "@pages/popup/Popup.css";
 
 const Popup = () => {
+  const [tabSize, setTabSize] = useState(8);
+  const onChangeTabSize = (e: ChangeEvent<HTMLInputElement>) => {
+    const newTabSize = Number(e.currentTarget.value);
+    setTabSize(newTabSize);
+    chrome.storage.sync.set({
+      tabSize: newTabSize,
+    });
+  };
+
+  useEffect(() => {
+    chrome.storage.sync.get(
+      {
+        tabSize: 8,
+      },
+      (items) => {
+        setTabSize(items.tabSize);
+      }
+    );
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <label>Tab size</label>
+        <input
+          type="number"
+          value={tabSize}
+          onChange={onChangeTabSize}
+          min={1}
+          max={8}
+        />
       </header>
     </div>
   );
